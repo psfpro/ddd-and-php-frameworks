54236aa145bcf99c8d8c37b499dbb6ebb446d6bd
Sergey Pantushin <psfpro@ya.ru>
2022-11-01 18:30:41 +0300

    symfony init

---
+++ migrations/Version20221029181817.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221029181817 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE "user" (id UUID NOT NULL, name VARCHAR(255) NOT NULL, email_address VARCHAR(255) NOT NULL, password_hash VARCHAR(255) NOT NULL, confirmation_token UUID NOT NULL, confirmation_is_passed BOOLEAN NOT NULL, PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP TABLE "user"');
+    }
+}
